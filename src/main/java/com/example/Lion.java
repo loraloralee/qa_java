9e10e726fe10ee95c8292883f297d112c9d037ed
Larisa Lee <loralee89-98@mail.ru>
2022-10-07 00:30:12 +0500

    Sprint_6

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -4,28 +4,26 @@ import java.util.List;
 
 public class Lion {
 
-    boolean hasMane;
+    private boolean hasMane;
+    private Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
+            this.feline=feline;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
+            this.feline=feline;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
-
-    Feline feline = new Feline();
-
     public int getKittens() {
         return feline.getKittens();
     }
-
     public boolean doesHaveMane() {
         return hasMane;
     }
-
     public List<String> getFood() throws Exception {
         return feline.getFood("Хищник");
     }
